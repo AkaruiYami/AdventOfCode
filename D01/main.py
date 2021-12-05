d9cbdec9cb7462c2cc5b4d42a1e8f45b30316538
AkaruiYami <shahrullfytri1@gmail.com>
2021-12-06 00:47:38 +0800

    Minor Fix and Reformating
    - Change parameter name for `m` to meaningful `l`.
    - Re-add print solution 1 that unintentionally deleted from the code.

--- D01/main.py
+++ D01/main.py
@@ -1,14 +1,17 @@
-def solution1(m):
+def solution1(l):
     """Count the number of increases from given list m."""
-    return [int(m[i]) > int(m[i - 1]) for i in range(1, len(m))].count(True)
+    return [int(l[i]) > int(l[i - 1]) for i in range(1, len(l))].count(True)
 
-def solution2(m):
+
+def solution2(l):
     """Count the number of increases from each sum of 3 number from give list"""
-    new_list = [int(m[i-2]) + int(m[i-1]) + int(m[i]) for i in range(2, len(m))]
+    new_list = [int(l[i - 2]) + int(l[i - 1]) + int(l[i]) for i in range(2, len(l))]
     return solution1(new_list)
+
 
 if __name__ == "__main__":
     with open("D01/input.txt") as f:
         _input = f.read().splitlines()
 
+    print(solution1(_input))
     print(solution2(_input))
