f5ff143c0fa9eaee44478c40ebbb1789d2491318
AkaruiYami <51882840+AkaruiYami@users.noreply.github.com>
2021-12-06 19:26:04 +0800

    Minor Changes
    - Use list comprehension inside get_instructions() functioni
    - Renaming constant variable

--- D02/main.py
+++ D02/main.py
@@ -1,43 +1,39 @@
-HORIZONTAL_MOVEMENT = {"up": -1, "down": 1}
+VERTICAL_COMMAND = {"up": -1, "down": 1}
 
 
-def get_instruction(l):
-    """Get a set of instructions (Movement, Value) from given list of strings."""
-    instructions = []
-    for item in l:
-        data = item.split()
-        instructions.append((data[0], int(data[1])))
-    return instructions
+def get_instructions(l):
+    return [tuple(_l.split()) for _l in l]
 
 
 def solution1(l):
-    """Return X * Y after following a set of instructions."""
-    position = [0, 0]
-    for move, value in get_instruction(l):
-        if move == "forward":
-            position[0] += value
+    x = 0
+    y = 0
+    instuctions = get_instructions(l)
+    for command, value in instuctions:
+        if command == "forward":
+            x += int(value)
         else:
-            position[1] += HORIZONTAL_MOVEMENT[move] * value
-    return position[0] * position[1]
+            y += VERTICAL_COMMAND[command] * int(value)
+    return x * y
 
 
 def solution2(l):
-    """Return X * Depth after following a set of instructions."""
+    x = 0
     aim = 0
     depth = 0
-    x = 0
-    for move, value in get_instruction(l):
-        if move == "forward":
-            x += value
-            depth += aim * value
+    instuctions = get_instructions(l)
+    for command, value in instuctions:
+        if command == "forward":
+            x += int(value)
+            depth += int(value) * aim
         else:
-            aim += HORIZONTAL_MOVEMENT[move] * value
+            aim += VERTICAL_COMMAND[command] * int(value)
     return x * depth
 
 
 if __name__ == "__main__":
-    with open("D02/input.txt") as f:
-        _input = f.read().splitlines()
+    with open("D02/input.txt") as file:
+        data = file.read().splitlines()
 
-    print(solution1(_input))
-    print(solution2(_input))
+    print(solution1(data))
+    print(solution2(data))
